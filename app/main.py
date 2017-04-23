0d5a3ed610f9a90c7aaa92045848c31b39367304
Alex Yang <aleozlx@gmail.com>
2017-04-23 13:51:21 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,55 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_path='/static')
 
-@app.route('/a.json')
-def _a():
-    return """{
-    "ds1/title1":
-        [{
-            "src": "ddjsk",
-            "prediction": 2
-        },{
-            "src": "vsdsaf",
-            "prediction": 3
-        },
-        {
-            "src": "vew",
-            "prediction": 7
-        },{
-            "src": "breds",
-            "prediction": 0
-        }],
-    "ds1/title2":
-        [{
-            "src": "vcaw",
-            "prediction": 2
-        },{
-            "src": "gewqt",
-            "prediction": 1
-        },
-        {
-            "src": "bkjie",
-            "prediction": 0
-        },{
-            "src": "iewoiiiknv",
-            "prediction": 7
-        }],
-    "ds2/title1":
-        [{
-            "src": "mskein",
-            "prediction": 2
-        },{
-            "src": "op3",
-            "prediction": 5
-        },
-        {
-            "src": "32532",
-            "prediction": 5
-        },{
-            "src": "lol",
-            "prediction": 6
-        }]
-}"""
+@app.route('/')
+def index():
+    return "awesome"
 
 if __name__ == '__main__':
     app.run()
